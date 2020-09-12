274cf8f40b82b937d4be30e3f139ce76f30d2b7a
SoupCake <61450397+SoupCake@users.noreply.github.com>
2020-09-12 13:56:23 +0800

    Add files via upload

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Assignment_FoodPartySystem.Areas.Identity.Data;
+using Assignment_FoodPartySystem.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Assignment_FoodPartySystem.Areas.Identity.IdentityHostingStartup))]
+namespace Assignment_FoodPartySystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection"),
+                        x => x.MigrationsAssembly("Assignment_FoodPartySystem")
+                        ));
+            });
+        }
+    }
+}
